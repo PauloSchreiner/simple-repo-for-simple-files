10ceaf984afed7e22d8e9aea8c5304453385c9ba
PauloSchreiner <51092525+PauloSchreiner@users.noreply.github.com>
2019-06-03 19:19:47 +0300

    try: prettify(browserTimer.py)

--- browserTimer.py
+++ browserTimer.py
@@ -9,12 +9,14 @@ from datetime import datetime
 import re
 
 argparse = argparse.ArgumentParser(description='Set a timer to open urls in your default browser.')
+
 argparse.add_argument('-w','--when',nargs='?',type=str,default=None,help='When to open the url. Enter the hour (0 to 23), followed by any non-digit (except space) and the minutes.')
 argparse.add_argument('-l','--minslater',nargs='?',type=int,default=1,help='Minutes to wait to open the url. Default: 1.')
 argparse.add_argument('-u','--url',nargs='?',type=str,default='',help='Enter a specific url to open.')
 argparse.add_argument('-a','--addurl',nargs='?',type=str,default='',help='Add an url to urlsFile.txt. To add multiple urls, set to "multiple".')
 argparse.add_argument('-o','--open',nargs='?',type=(lambda x:(str(x).lower() == 'true')),default=True,help='To open a random url from urlsFile.txt or not. Default: True.')
 argparse.add_argument('-r','--remove',nargs='?',type=(lambda x:(str(x).lower() == 'true')),default=False,help='to remove opened url from urlsFile.txt or not. Default: False.')
+
 args = argparse.parse_args()
 
 urlsFileName = 'urlsFile.txt'
@@ -29,6 +31,7 @@ print(timeNow[0]+':'+timeNow[1])
 
 #If given url is not in a url format or an empty string, format it to be so.
 urlRegex = '(https://)*www[.].+[.]com.*'
+
 def toUrlFormat(url):
     if (not re.search(urlRegex, url)) and url != '':
         url = 'https://www.{}.com'.format(url)
@@ -36,20 +39,25 @@ def toUrlFormat(url):
 
 toUrlFormat(args.url)
 
+
 #Accept multiple urls and add to urlsFile.txt
+
 if args.addurl == 'multiple':
     urlsList = []
     print('Enter the urls. To stop, enter "stop".')
+
     while True:
         inpUrl = input('Url to append: ')
         if inpUrl == 'stop':
             break
         urlsList.append(toUrlFormat(inpUrl))
+ 
     with open(urlsFileName,'a') as urlsFile:
         for url in urlsList:
             if url:
                 urlsFile.write(url+'\n')
     print('Urls added successfully!')
+    
 else: #Append args.addurl to urlsFile.txt
     if args.addurl:
         args.addurl = toUrlFormat(args.addurl)
@@ -57,6 +65,8 @@ else: #Append args.addurl to urlsFile.txt
             urlsFile.write(args.addurl+'\n')
             print('Url added successfully!')
 
+
+            
 #Only sleep and open url if args.open == True
 if args.open == True:
     #If args.when exists, sleep for the amount os seconds until given time
@@ -64,6 +74,7 @@ if args.open == True:
         when_list = re.findall('\d+', args.when)
         when = ((int(when_list[0])*60)+int(when_list[1]))*60
         toSleep = when-SecNow()
+
         if toSleep < -30000: #To accept 12h formatted time. Not precise, it is preferable to aways use 24hs
             toSleep += 43200
         elif toSleep < 0:
@@ -75,6 +86,7 @@ if args.open == True:
     else:
         print('Sleeping for {} minute(s).'.format(args.minslater))
         time.sleep(args.minslater*60)
+    
     #If args.url exists, let the string content be equal to it.
     if args.url:
         content = args.url
@@ -82,12 +94,15 @@ if args.open == True:
         with open(urlsFileName,'r') as urlsFile:
             fileContents = urlsFile.read().split('\n')
             content = random.choice(fileContents)
+    
     try:
         print('Opening url.')
         time.sleep(0.3)
         webbrowser.open(content)
+    
     except Exception as e:
         print('An error occured while opening the url.\nError message:',e)
+    
     if args.remove:
         with open(urlsFileName,'r+') as urlsFile:
             data = urlsFile.read()
